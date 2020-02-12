a6f9bc53b1ca6c5056cb1102c9a3443a8d2e5487
Zoey <zcolglazier@wisc.edu>
2020-02-12 15:22:06 -0600

    updates for adaptedTutorial

--- js/adaptedTutorial.js
+++ js/adaptedTutorial.js
@@ -1,93 +1,57 @@
 //I used .setView to center my map on a specific location when it first loads.
-var mymap = L.map('mapid').setView([39.75621, -104.99404], 5);
+var mymap;
+//let's make a map
+function createMap(){
+    //create the map
+    mymap = L.map('map', {
+        center: [20, 0],
+        zoom: 2
+    });
 
-var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 19
-}).addTo(mymap);
-//.addTo adds things to the specified object - through this script it is used to append to my map
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(mymap);
+
+	getData();
+};
+
 function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
-}
-//.bindPopup attaches a popup text to the object it references, which can then be accessed via click.
-
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
-
-var myLines = [{
-    "type": "LineString",
-    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-}, {
-    "type": "LineString",
-    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-}];
-
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
-};
-//.geoJSON is taking the lines we hard-coded and creating a geoJSON layer, as well as styling it. .addTo adds to the map
-L.geoJSON(myLines, {
-    style: myStyle
-}).addTo(mymap);
-
-var states = [{
-    "type": "Feature",
-    "properties": {"party": "Republican"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-104.05, 48.99],
-            [-97.22,  48.98],
-            [-96.58,  45.94],
-            [-104.03, 45.94],
-            [-104.05, 48.99]
-        ]]
-    }
-}, {
-    "type": "Feature",
-    "properties": {"party": "Democrat"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-109.05, 41.00],
-            [-102.06, 40.99],
-            [-102.03, 36.99],
-            [-109.04, 36.99],
-            [-109.05, 41.00]
-        ]]
-    }
-}];
-
-L.geoJSON(states, {
-    style: function(feature) {
-        switch (feature.properties.party) {
-            case 'Republican': return {color: "#ff0000"};
-            case 'Democrat':   return {color: "#0000ff"};
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
         }
-    }
-}).addTo(mymap);
-
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
+        layer.bindPopup(popupContent);
+    };
 };
+
+//function to retrieve the data and place it on the map
+function getData(mymap){
+    //load the data
+    $.getJSON("data/MegaCities.geojson", function(response){
+		var geojsonMarkerOptions = {
+				radius: 8,
+				fillColor: "#ff7800",
+				color: "#000",
+				weight: 1,
+				opacity: 1,
+				fillOpacity: 0.8
+			};
+
+			L.geoJson(response, {
+					onEachFeature: onEachFeature
+				}).addTo(mymap);
+			L.geoJson(response, {
+					pointToLayer: function (feature, latlng){
+							return L.circleMarker(latlng, geojsonMarkerOptions);
+					}
+    }).addTo(mymap);
+	});
+};
+
+
+//.ready will execute the function it requires once the document has all the data it needs.
+$(document).ready(createMap);
