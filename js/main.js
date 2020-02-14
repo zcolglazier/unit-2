0b9c9a428d44d3ac7ffaca17baf706cfbce66dd1
zcolglazier <zcolglazier@wisc.edu>
2020-02-14 14:43:28 -0600

    Leaflet_Lab_Part 1

---
+++ js/main.js
@@ -0,0 +1,55 @@
+//I used .setView to center my map on a specific location when it first loads.
+var mymap;
+//let's make a map
+function createMap(){
+    //create the map
+    mymap = L.map('map', {
+        center: [20, 0],
+        zoom: 2
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(mymap);
+
+	getData();
+};
+
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+//function to retrieve the data and place it on the map
+function getData(mymap){
+    //load the data
+    $.getJSON("data/Pop_Est_Change_US.geojson", function(response){
+		var geojsonMarkerOptions = {
+				radius: 8,
+				fillColor: "#ff7800",
+				color: "#000",
+				weight: 1,
+				opacity: 1,
+				fillOpacity: 0.8
+			};
+
+			L.geoJson(response, {
+					onEachFeature: onEachFeature,
+					pointToLayer: function (feature, latlng){
+							return L.circleMarker(latlng, geojsonMarkerOptions)
+				}
+			}).addTo(mymap);
+		});
+}
+
+
+//.ready will execute the function it requires once the document has all the data it needs.
+$(document).ready(createMap);
